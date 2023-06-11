6207ff3e40d9eebe94c694960813c502cebc8212
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-06-11 15:20:11 +0100

    edit

--- Frontend/app/profile/components/Friends.tsx
+++ Frontend/app/profile/components/Friends.tsx
@@ -4,7 +4,70 @@ const Friends = () => {
         <div>
 
 <h3 className="first_h3">Friends</h3>
-            <div className="Friends"></div>
+            <div className="Friends">
+
+                <div className="friend">
+                    <div className="inside_friend">
+                        <img src="/avatar.png" alt="" />
+                        <p>selhanda</p>
+                    </div>
+
+                    <p className="enligne">
+                        Active
+                    </p>
+                    
+                </div>
+
+                <div className="friend">
+                    <div className="inside_friend">
+                        <img src="/avatar.png" alt="" />
+                        <p>jdslwie</p>
+                    </div>
+
+                    <p className="enligne">
+                        Active
+                    </p>
+                    
+                </div>
+
+
+                <div className="friend">
+                    <div className="inside_friend">
+                        <img src="/avatar.png" alt="" />
+                        <p>salamane</p>
+                    </div>
+
+                    <p className="enligne">
+                        Active
+                    </p>
+                    
+                </div>
+
+                <div className="friend">
+                    <div className="inside_friend">
+                        <img src="/avatar.png" alt="" />
+                        <p>mabdelou</p>
+                    </div>
+
+                    <p className="enligne">
+                        Active
+                    </p>
+                    
+                </div>
+
+
+                <div className="friend">
+                    <div className="inside_friend">
+                        <img src="/avatar.png" alt="" />
+                        <p>mabdelou</p>
+                    </div>
+
+                    <p className="enligne">
+                        Active
+                    </p>
+                    
+                </div>
+            </div>
         </div>
         
     );
