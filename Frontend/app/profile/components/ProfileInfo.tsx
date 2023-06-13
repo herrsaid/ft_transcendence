2702e7508253b215dfacf8108a560e3b01d1faea
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-06-13 13:05:36 +0100

    edit profile

--- Frontend/app/profile/components/ProfileInfo.tsx
+++ Frontend/app/profile/components/ProfileInfo.tsx
@@ -5,21 +5,36 @@ const ProfileInfo = () => {
         <div>
                 <div className="info">
                                 <div className="inside_info_p">
-                                <p>
-                                <span className="group_wins">Wins:<span className="wins_span">50</span></span>
-                                <span className="group_wins">Rank:<span className="wins_span">Gold-P3</span></span>
-                                </p>
+                                <div className="info_b_one">
+                                    <div>
+                                    <span className="group_wins">Wins: <span className="wins_span">50</span></span>
+                                    </div>
+                                    <div>
+                                    <span className="group_wins">Rank: <span className="wins_span">Gold-P3</span></span>
+                                    </div>
+                                </div>
+                                
+                                
+                                
 
-                                <p>
-                                <span className="group_wins">Loss:<span className="wins_span">20</span></span>
-                                <span className="group_wins">Location:<span className="wins_span">morroco</span></span>
-                                </p>
+                                <div className="info_b_one">
 
-
-                                <p>
-                                <span className="group_wins">Total Game:<span className="wins_span">77</span></span>
-                                <span className="group_wins">Status:<span className="wins_span">Available</span></span>
-                                </p>
+                                    <div>
+                                    <span className="group_wins">Loss: <span className="wins_span">20</span></span>
+                                    </div>
+                                    <div>
+                                    <span className="group_wins">Location: <span className="wins_span">morroco</span></span>
+                                    </div>
+                                </div>
+                                
+                                <div className="info_b_one">
+                                    <div>
+                                    <span className="group_wins">Total Game: <span className="wins_span">77</span></span>
+                                    </div>
+                                    <div>
+                                    <span className="group_wins">Status: <span className="wins_span">Available</span></span>
+                                    </div>
+                                </div>
 
 
                                 <p>
