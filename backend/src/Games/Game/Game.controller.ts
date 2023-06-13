faf3642b92a3f2322a93569524462b1abfdf42a2
mabdelou <programming.abdu@gmail.com>
2023-06-14 00:33:13 +0100

    add some logic business at Game.Controller

--- backend/src/Games/Game/Game.controller.ts
+++ backend/src/Games/Game/Game.controller.ts
@@ -1,15 +1,65 @@
-import { Controller, Get, Post, Delete, Patch, Param } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Patch,
+  Param,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { GameEtity } from './Game.Entity';
 @Controller('Games')
 export class GameController {
+  Game: GameEtity[] = [];
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  GetAllGames(): GameEtity[] {
+    return this.Game;
+  }
   @Get(':gamename')
-  GetGameName(@Param('gamename') gamename: string): string {
-    if (
-      gamename != 'PingPong' &&
-      gamename != 'Chess' &&
-      gamename != 'CardGames'
-    )
-      return `there is no game in our website  have that name "${gamename}"`;
-    return `hello from GameControler the game is ${gamename}`;
+  @HttpCode(HttpStatus.OK)
+  GetGame(@Param('gamename') gamename: string): string {
+    const ret = this.Game.find((game) => game.gamename === gamename);
+    if (ret === undefined) return `can't GetGame this game not found`;
+    return `gamename is ${ret.gamename}\n gameid is ${ret.id}\n gamestatus is ${ret.status}`;
+  }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  CreateGame(@Body() body: GameEtity): string {
+    if (this.Game.find((game) => game.id === body.id)) return 'id alredy exist';
+    else if (this.Game.find((game) => game.gamename === body.gamename))
+      return 'gamename alredy exist';
+    this.Game.push(body);
+    return 'game created successfully';
+  }
+
+  @Patch(':gamename')
+  @HttpCode(HttpStatus.OK)
+  UpdateGame(
+    @Param('gamename') gamename: string,
+    @Body() body: GameEtity,
+  ): string {
+    const index = this.Game.findIndex((game) => game.gamename === gamename);
+    console.log(body);
+    if (this.Game[index] === undefined)
+      return `can't UpdatGame: this game not found`;
+    else if (this.Game.find((game) => game.id === body.id))
+      return 'id alredy exist';
+    else if (this.Game.find((game) => game.gamename === body.gamename))
+      return 'gamename alredy exist';
+    // this.Game[index] = { ...this.Game[index], ...body };
+    this.Game[index].id = body.id;
+    this.Game[index].gamename = body.gamename;
+    this.Game[index].status = body.status;
+
+    return 'game Update successfully';
+  }
+  @Delete(':gamename')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  RemoveGame(@Param('gamename') gamename: string): string {
+    this.Game = this.Game.filter((game) => game.gamename !== gamename);
+    return 'game Removed successfully';
   }
 }
--- backend/src/Games/Games.controller.ts
+++ backend/src/Games/Games.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class GamesController {
-  @Get('Games')
-  newhello(): string {
-    return 'hello from GamesController';
-  }
+  // @Get('Games')
+  // newhello(): string {
+  //   return 'hello from GamesController';
+  // }
 }
