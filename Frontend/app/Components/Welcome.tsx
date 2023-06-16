f04ee67949ef35e1795fc8cc863ed0682a19f6b5
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-06-16 14:13:38 +0100

    edit login page

--- Frontend/app/Components/Welcome.tsx
+++ Frontend/app/Components/Welcome.tsx
@@ -1,19 +1,17 @@
 import Link from 'next/link'
 import './Welcome.css'
-import SideNavBar from './SideNavBar/SideNavBar';
-
 
 export default function Welcome()
 {
     return(
         <>
-        <SideNavBar/>
         <div className="w">
                 <div className="container">
                 <div className="main_home">
-                    <h2>Play PingPong With Your Friend</h2>
-                    <Link href="/Games">
-                        <button>Play Now</button>
+                    <h2>Welcome to legends</h2>
+                    <p>Play PingPong with your friends online</p>
+                    <Link href="http://localhost:1337/api/auth/42">
+                        <button>Sign in with 42</button>
                     </Link>
                     
                 </div>
