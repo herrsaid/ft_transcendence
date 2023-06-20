f557080c416969654b469e9ae73b181214928271
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-06-20 15:53:36 +0100

    edit intra login auth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,21 +1,20 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authservice: AuthService) {}
   @Get('42')
   @UseGuards(AuthGuard('42'))
   async loginWith42() {
-    // The user will be redirected to the 42 login page
-    // and then to the callback URL configured in the AuthStrategy.
-    // The actual authentication process is handled by the passport-42 strategy.
+    
   }
 
   @Get('42/callback')
   @UseGuards(AuthGuard('42'))
   async loginWith42Callback(@Req() req: Request, @Res() res: Response) {
-    // Successful authentication, redirect to the desired route
-    res.redirect('/dashboard');
+      return this.authservice.loginIntra42(req);
   }
 }
