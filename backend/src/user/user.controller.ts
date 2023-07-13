035c18161721a27c30ecba5ce233e1e2231ccebb
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-07-13 16:06:59 +0100

    get user data in frontend

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,45 +1,26 @@
-import { Body, Controller, Get, Param, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Request, Res, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtService } from '@nestjs/jwt';
+
 
 @Controller('user')
 export class UserController {
 
-    constructor(private readonly userService:UserService)
+    constructor(private readonly userService:UserService, private jwtService: JwtService)
     {}
 
-
+    
     @UseGuards(AuthGuard)
     @Get('me')
-    profileInfo()
+    profileInfo(@Req() req:Request)
     {
+        const tocken_part = req.headers['authorization'].split(' ')
+        const decodedJwtAccessToken = this.jwtService.decode(tocken_part[1]);
         
-        // return req.headers.cookie;
-        return [
-            {
-                id:'1',
-                username:'selhanda',
-                loss:0
-            },
-            {
-                id:'2',
-                username:'selhanda',
-                loss:0
-            },
-            {
-                id:'3',
-                username:'selhanda',
-                loss:0
-            },
-            {
-                id:'4',
-                username:'selhanda',
-                loss:0
-            },
-            
+        return this.userService.findOne(decodedJwtAccessToken['sub'])
 
-        ]
     }
 
     @Get(':id')
@@ -55,3 +36,5 @@ export class UserController {
     }
    
 }
+
+
