c489a556603edf518f973ca9c00c8be06fc80764
said lamane <thisfor1337@gmail.com>
2023-07-20 12:21:09 +0100

    add modal

--- Frontend/app/Components/Header.tsx
+++ Frontend/app/Components/Header.tsx
@@ -2,10 +2,33 @@
 import './Header.css'
 import { useState, useEffect } from 'react'
 
+function Login()
+{
+    const [open, setOpen] = useState(false);
+    return(
+        <div>
+            <img onClick={()=>{setOpen(!open)}} src="https://randomuser.me/api/portraits/women/3.jpg" alt="test" />
+            {open && <div className='modal'>
+                <ul>
+                    <li>
+                        <h1>Profile</h1>
+                    </li>
+                    <li>
+                        <h1>Setting</h1>
+                    </li>
+                    <li>
+                        <h1>Logout</h1>
+                    </li>
+                </ul>
+            </div>}
+        </div>
+    )
+}
 
 export default  function Header()
 {
     const [login, setLogin] = useState(<button className='login'>Login</button>);
+    useEffect(()=>{setLogin(<Login></Login>)},[])
     return(
         <div className="header">
             <div className='logo-search'>
@@ -14,7 +37,6 @@ export default  function Header()
             </div>
             <div className='profile'>
                 <div className='profile-img'>
-                    {/* <img src="https://randomuser.me/api/portraits/women/3.jpg" alt="test" /> */}
                     {login}
                 </div>
             </div>
