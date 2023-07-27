c4ad2057849bbad335bc58e33b8b5980c69ced13
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-07-27 19:14:48 +0100

    edit

--- Frontend/app/Components/SearchInput.tsx
+++ Frontend/app/Components/SearchInput.tsx
@@ -27,7 +27,7 @@ export default function SearchInput()
     
             onChange={(event) => setsearchQuery(event.target.value)}
             className="px-5 py-1 w-2/3 sm:px-5 sm:py-3 flex-1 text-zinc-200 bg-zinc-800
-            focus:bg-black rounded-full focus:outline-none" placeholder="Search for user"/>
+            focus:bg-black rounded-full focus:outline-none " placeholder="Search for user"/>
 
         </form>
     );
