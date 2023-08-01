3e3dad4c81b742a44634004087e016ffeb9671c3
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-08-01 19:20:34 +0100

    Edit

--- Frontend/app/profile/components/OneHistory.tsx
+++ Frontend/app/profile/components/OneHistory.tsx
@@ -1,11 +1,13 @@
 import { Avatar, AvatarBadge } from "@chakra-ui/react";
 import Cookies from 'js-cookie';
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import useSWR from "swr"
 
 const OneHistory = () => {
     const router = useRouter();
     let myFriends;
+    let new_src_img:string;
     
     const fetchFriends = async (url:string) => {
         const res = await fetch(url, {
@@ -32,6 +34,8 @@ const OneHistory = () => {
     if (data)
     {
         myFriends = data.map(friend => {
+            if (friend.is_profile_img_updated)
+                new_src_img = process.env.NEXT_PUBLIC_BACK_IP + "/user/profile-img/" + friend.profile_img;
             return <div className="victory_div" key={friend.id}>
             <div className="info_victory">
                 <p>Score:    2036</p>
@@ -43,9 +47,13 @@ const OneHistory = () => {
                 <p>8 vs 6</p>
             </div>
         
-            <div className="game_victory"><div className="game_victory" ><Avatar  size='md' name={friend.username} src={friend.profile_img} >
+            <div className="game_victory"><div className="game_victory" >
+                <Link href={`/user?username=${friend.username}`}>
+                <Avatar  size='md' name={friend.username} src={friend.is_profile_img_updated ? new_src_img : friend.profile_img} >
             <AvatarBadge boxSize='1em' bg='green.500' borderColor='#18184a'/>
             </Avatar>
+                </Link>
+                
              </div>
              </div>
 </div>
