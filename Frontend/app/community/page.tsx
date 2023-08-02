a15f6fd8367178237996c2dd7e3f74fd9e03cb6d
said lamane <thisfor1337@gmail.com>
2023-08-02 17:30:50 +0100

    finally chat

--- Frontend/app/community/page.tsx
+++ Frontend/app/community/page.tsx
@@ -10,6 +10,9 @@ import Cookies from 'js-cookie';
 import {socket} from '../socket/socket'
 import Messages from '../messages/messages';
 import  useSWR  from 'swr';
+import { useRouter } from 'next/navigation';
+
+
 const Reciver = createContext(0);
 
 function Message(props: any)
@@ -96,36 +99,49 @@ export default function Community()
 
 function Chat()
 {
+    // const router = useRouter();
     const con = useContext(Reciver);
     const inputRef = useRef(null);
     const [val, setValue] = useState('')
     const [messages, setMessages] = useState([]);
-    let zabi
     const myId = sessionStorage.getItem('userId')
-    let msg: any[] = messages
     console.log('id', myId)
-    const fetcher = async (url:string) => {
-        const res = await fetch(url,
-        {
+    // const fetcher = async (url:string) => {
+        //     const res = await fetch(url,
+        //     {
+            //         method: 'GET', headers:{
+                
+                //         Authorization: `Bearer ${Cookies.get('access_token')}`
+                //     }});
+                //     // if (res.status == 401)
+                //     //     router.replace("/login");
+                
+                //     if (!res.ok)
+                //         throw new Error("fetch error");
+                //     return res.json();
+                // }
+                
+                // const {data, error, isLoading} = useSWR(`${process.env.NEXT_PUBLIC_BACK_IP}/messages?id=${myId}`, fetcher)
+                const [data, setData] = useState([]);
+    useEffect(()=> {
+        fetch(`${process.env.NEXT_PUBLIC_BACK_IP}/messages?id=${myId}`,{
             method: 'GET', headers:{
-            Authorization: `Bearer ${Cookies.get('access_token')}`
-        }});
-        if (!res.ok)
-            throw new Error("fetch error");
-        return res.json();
-    }
-
-    const {data, error, isLoading} = useSWR(`${process.env.NEXT_PUBLIC_BACK_IP}/messages?id=${myId}`, fetcher)
+                
+                Authorization: `Bearer ${Cookies.get('access_token')}`
+            }
+        }).then((response) => response.json()).then(data => setData(data))
+    }, [])
     console.log(data);
     const [rerender, setRerender] = useState(1);
-    console.log('msg = ', msg);
+    let msg: any[] = data
     useEffect (() =>{
-        socket.on('message', (data:any) => {
-            if (data != '')
+        socket.on('message', (message:any) => {
+            if (message != '' && msg)
             {
-                data.push({src:data.src, key:(Date.now() + Math.random()),content:data.content, dst:data.dst, class:'you'})
+                // msg.push({src:message.src, content:message.content, dst:message.dst})
+                setData(old => [...old, {src:message.src, content:message.content, dst:message.dst}]);
                 setRerender(Math.random());
-                console.log(msg)
+                console.log(data)
                 console.count('useEffecte')
             }
         })
@@ -137,17 +153,11 @@ function Chat()
         if (val != '')
         {
             socket.emit('message', {src:sessionStorage.getItem('userId'),dst:con.id, content:val});
-            // data.push({key:(Date.now() + Math.random()) , reciver:con.id, content:val, class:'me', src:myId, dst:con.id})
-            data.push({key:Math.random(), src:myId, dst:con.id, content:val, class:'me'});
-            setMessages(msg);
+            data.push({src:myId, dst:con.id, content:val});
             console.log(con.id);
             setValue('');
         }
     };
-    if (isLoading)
-        return (
-            <> loading </>
-        )
     return(
         <div className='chat-box'>
             <div className='chat-info'>
--- backend/src/websock/websock.gateway.ts
+++ backend/src/websock/websock.gateway.ts
@@ -21,7 +21,6 @@ export class WebsockGateway {
   server: Server;
 
   online_users = [];
-  msg
   handleConnection(socket: Socket)
   {
     try{
@@ -29,16 +28,14 @@ export class WebsockGateway {
       const payload = jwt.verify(token, 'complexkey3884-asgfgsd,s33003400mmdma-434-4das111!!!!!+++')
       this.online_users.push({socket_id:socket.id,user_id:payload.id})
       console.log(this.online_users, this.online_users.length)
-      console.log(this.MessageService.getMessages(payload.id))
-      this.msg = this.MessageService.getMessages(payload.id)
     }catch(error){
       console.log('l9lawi ladkhlti')
-      socket.disconnect();   
+      socket.disconnect(); 
     }
   }
   handleDisconnect(socket: Socket)
   {
-    // console.log(socket.id, this.online_users.findIndex(obj => obj.socket_id == socket.id))
+    console.log('disconnected ', socket.id)
     this.online_users.splice(this.online_users.findIndex(obj => obj.socket_id == socket.id), 1);
   }
   getSocketId(dst_id: number)
@@ -53,9 +50,7 @@ export class WebsockGateway {
   @SubscribeMessage('message')
   handleMessage(client: Socket, payload: any): string {
     this.MessageService.create(payload);
-    // this.MessagesService.create(payload)
     let dst
-    console.log(this.msg.Messages);
     try{
       dst = this.getSocketId(payload.dst);
     }catch (error)
