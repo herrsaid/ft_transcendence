3cad23082c66a06544d12f5f815121e2f7e66b86
mabdelou <programming.abdu@gmail.com>
2023-08-03 08:08:25 +0100

    let script update backend/.env file

--- Frontend/app/Game/Online/Socket/start_game_socket.ts
+++ Frontend/app/Game/Online/Socket/start_game_socket.ts
@@ -6,16 +6,17 @@
 /*   By: mabdelou <mabdelou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 18:39:14 by mabdelou          #+#    #+#             */
-/*   Updated: 2023/08/02 18:39:15 by mabdelou         ###   ########.fr       */
+/*   Updated: 2023/08/02 19:39:39 by mabdelou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { io } from 'socket.io-client';
-const URL:string = process.env.GAME_PLAYER_I_SOCKET!;
-const URL2:string = process.env.GAME_PLAYER_II_SOCKET!;
-export const player1 = io(URL, {extraHeaders:{
-        'Access-Control-Allow-Origin': "*"
-    }});
+
+const URL:string = "10.11.10.2:1340";
+const URL2:string = "10.11.10.2:1341";
+    export const player1 = io(URL, {extraHeaders:{
+            'Access-Control-Allow-Origin': "*"
+        }}); 
 export const player2 = io(URL2, {extraHeaders:{
         'Access-Control-Allow-Origin': "*"
     }});
--- script.sh
+++ script.sh
@@ -6,18 +6,18 @@
 #    By: mabdelou <mabdelou@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/02 10:24:50 by mabdelou          #+#    #+#              #
-#    Updated: 2023/08/02 18:53:30 by mabdelou         ###   ########.fr        #
+#    Updated: 2023/08/02 19:46:36 by mabdelou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 MachineLine=$(ifconfig | grep 10.11 | awk '{printf $2}')
-EnvIPath="$HOME/goinfre/ft_transcendence/Frontend/.env.local"
-
+FrontendEnvIPath="$HOME/goinfre/ft_transcendence/Frontend/.env.local"
+BackendEnvIPath="$HOME/goinfre/ft_transcendence/backend/.env"
 if [ -d "$HOME/goinfre/ft_transcendence" ]; then
 echo "updating .env"
-cat << EDF > $EnvIPath
+cat << EDF > $FrontendEnvIPath
 SOCKET_URL=http://$MachineLine:3030
 NEXT_PUBLIC_SOCKET=http://$MachineLine:3030
 NEXT_PUBLIC_BACK_IP=http://$MachineLine:1337
@@ -26,11 +26,26 @@ AUTO_MATCH_SOCKET=http://$MachineLine:1339
 GAME_PLAYER_I_SOCKET=http://$MachineLine:1340
 GAME_PLAYER_II_SOCKET=http://$MachineLine:1341
 EDF
+cat << EDF > $BackendEnvIPath
+DATABASE_NAME="testDB"
+DATABASE_HOST="localhost"
+DATABASE_USERNAME="postgres"
+DATABASE_PASSWORD="postgres"
+FRONT_IP=http://$MachineLine:3000
+CLIENT_ID=601566209551-rrmmg509qmjtgrmmgei05kqs9a2jjs6j.apps.googleusercontent.com
+CLIENT_SECRET=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
+CLIENT_ID_42=u-s4t2ud-c23c837b8aa6b7a4f906cc1ec6663ee2f70ab478c53bd43763996d7c479c8d26
+CLIENT_SECRET_42=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
+TWO_FACTOR_AUTHENTICATION_APP_NAME=ft_trand
+JWT_ACCESS_TOKEN_SECRET=complexkey3884-asgfgsd,s33003400mmdma-434-4das111!!!!!+++
+JWT_ACCESS_TOKEN_EXPIRATION_TIME=30
+INTRA_CALL_BACK=http://$MachineLine/auth/42/callback
+EDF
 echo "done!"
 else
 cd $HOME/goinfre/ ; git clone git@github.com:herrsaid/ft_transcendence.git
 echo "create repo directory && update .envfile"
-cat << EDF > $EnvIPath
+cat << EDF > $FrontendEnvIPath
 SOCKET_URL=http://$MachineLine:3030
 NEXT_PUBLIC_SOCKET=http://$MachineLine:3030
 NEXT_PUBLIC_BACK_IP=http://$MachineLine:1337
@@ -38,6 +53,21 @@ NEXT_PUBLIC_BACK_IP_G=http://$MachineLine:1337
 AUTO_MATCH_SOCKET=http://$MachineLine:1339
 GAME_PLAYER_I_SOCKET=http://$MachineLine:1340
 GAME_PLAYER_II_SOCKET=http://$MachineLine:1341
+EDF
+cat << EDF > $BackendEnvIPath
+DATABASE_NAME="testDB"
+DATABASE_HOST="localhost"
+DATABASE_USERNAME="postgres"
+DATABASE_PASSWORD="postgres"
+FRONT_IP=http://$MachineLine:3000
+CLIENT_ID=601566209551-rrmmg509qmjtgrmmgei05kqs9a2jjs6j.apps.googleusercontent.com
+CLIENT_SECRET=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
+CLIENT_ID_42=u-s4t2ud-c23c837b8aa6b7a4f906cc1ec6663ee2f70ab478c53bd43763996d7c479c8d26
+CLIENT_SECRET_42=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
+TWO_FACTOR_AUTHENTICATION_APP_NAME=ft_trand
+JWT_ACCESS_TOKEN_SECRET=complexkey3884-asgfgsd,s33003400mmdma-434-4das111!!!!!+++
+JWT_ACCESS_TOKEN_EXPIRATION_TIME=30
+INTRA_CALL_BACK=http://$MachineLine/auth/42/callback
 EDF
 echo "done!"
 fi
