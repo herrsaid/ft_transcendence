59bcf6c8155f7f6bfbba7ff748025fc8b1a9584d
Salah eddine Elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-08-10 18:04:12 +0100

    edit the bottombar

--- Frontend/app/(root)/Components/SideNavBar_Res/SideNavBar_Res.tsx
+++ Frontend/app/(root)/Components/SideNavBar_Res/SideNavBar_Res.tsx
@@ -10,9 +10,9 @@ export default function SideNavBar_Res()
 {
     return(
         <>
-        <nav className="fixed bottom-0 lg:bottom-0 w-full overflow-hidden z-50">
+        <nav className="fixed bottom-2 lg:bottom-2 w-full overflow-hidden z-50">
             <div className="container mx-auto">
-                <div className="w-full h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto 
+                <div className="w-full h-[96px] bg-black/20 backdrop-blur rounded-full max-w-[460px] mx-auto 
                 px-5 flex justify-between text-2xl text-white/50 items-center
                 ">
                     <Link href='/' className="rounded-full cursor-pointer w-[60px] h-[60px] flex items-center justify-center hover:bg-gradient-to-br from-purple-600 to-indigo-800"><IoHome size={30} /></Link>
