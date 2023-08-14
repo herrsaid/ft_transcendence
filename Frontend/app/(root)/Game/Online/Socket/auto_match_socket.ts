080a0224731b452f89fc62d25681ac701e449cc4
Mohammed Abdelouafi <mabdelou@c3r10p5.1337.ma>
2023-08-14 14:28:32 +0100

    small update on script.sh

--- Frontend/app/(root)/Game/Online/Socket/auto_match_socket.ts
+++ Frontend/app/(root)/Game/Online/Socket/auto_match_socket.ts
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://10.11.10.4:1339', {extraHeaders:{
+export const socket = io('http://:1339', {extraHeaders:{
         'Access-Control-Allow-Origin': "*"
     }});
--- script.sh
+++ script.sh
@@ -6,17 +6,16 @@
 #    By: mabdelou <mabdelou@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/08/02 10:24:50 by mabdelou          #+#    #+#              #
-#    Updated: 2023/08/05 21:22:01 by mabdelou         ###   ########.fr        #
+#    Updated: 2023/08/14 14:27:58 by mabdelou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
 MachineLine=$(ifconfig | grep 10.11 | awk '{printf $2}')
-FileIPath="$HOME/goinfre/ft_transcendence/Frontend/app/Game/Online/Socket/auto_match_socket.ts"
-FileIIPath="$HOME/goinfre/ft_transcendence/Frontend/app/Game/Online/Socket/start_game_socket.ts"
-
-if [ -d "$HOME/goinfre/ft_transcendence" ]; then
+if [ -d "$HOME/Desktop/ft_transcendence" ]; then
+	FileIPath="$HOME/Desktop/ft_transcendence/Frontend/app/(root)/Game/Online/Socket/auto_match_socket.ts"
+FileIIPath="$HOME/Desktop/ft_transcendence/Frontend/app/(root)/Game/Online/Socket/start_game_socket.ts"
 cat << EDF > $FileIPath
 import { io } from 'socket.io-client';
 EDF
@@ -42,7 +41,9 @@ cat << EDF >> $FileIIPath
 EDF
 echo "done!"
 else
-cd $HOME/goinfre/ ; git clone git@github.com:herrsaid/ft_transcendence.git
+cd $HOME/Desktop/ ; git clone git@github.com:herrsaid/ft_transcendence.git
+FileIPath="$HOME/Desktop/ft_transcendence/Frontend/app/(root)/Game/Online/Socket/auto_match_socket.ts"
+FileIIPath="$HOME/Desktop/ft_transcendence/Frontend/app/(root)/Game/Online/Socket/start_game_socket.ts"
 echo "create repo directory && update .envfile"
 cat << EDF > $FileIPath
 import { io } from 'socket.io-client';
@@ -71,41 +72,11 @@ echo "done!"
 fi
 
 [ `uname -s` != "Darwin" ] && return
-open -a "Visual Studio Code" $HOME/goinfre/ft_transcendence
-osascript << EDF &>/dev/null
---run Docker Aplication
 
-	tell application "Docker"
-	    activate
-	end tell
-
-tell application "iTerm 2.app"
-	--Create first initial window
-
-	    create window with default profile
-
-    --Create secound initial window
-
-	    create window with default profile
-
-	--Send a command to the first session
-
-        tell session 1 of tab 1 of  window 1
-            write text "cd $HOME/goinfre/ft_transcendence/Frontend ; npm i ; npm run dev"
-        end tell
-
-	--Send a command to the secound session
-
-        tell session 1 of tab 1 of  window 2
-            write text "sleep 90; cd $HOME/goinfre/ft_transcendence/backend ; npm i ; npm run start:dev"
-        end tell
-
-	--Select the first tab
-
-	tell session 1
-		select
-	end tell
-end tell
-
-EDF
+if [ -d "/nfs/homes/mabdelou/.local/share/applications/code"]; then
+/nfs/homes/mabdelou/.local/share/applications/code/code ~/Desktop/ft_transcendence
+fi
+gnome-terminal --tab --command="zsh -c 'cd $Home/Desktop/ft_transcendence/Frontend ; npm i ; npm run dev'"
+gnome-terminal --tab --command="zsh -c 'cd $Home/Desktop/ft_transcendence/backend ;npm i ; sleep 90; npm start:run dev'"
+gnome-terminal --tab --cpmmand="zsh -c 'cd $Home/Desktop/ft_transcendence'; docker-compose up"
 exit
