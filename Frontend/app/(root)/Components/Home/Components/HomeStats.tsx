d4b8824c63a2c8491a93b8b19a6349e1f7f1b930
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-08-26 18:06:31 +0100

    some changes in the home pages

--- Frontend/app/(root)/Components/Home/Components/HomeStats.tsx
+++ Frontend/app/(root)/Components/Home/Components/HomeStats.tsx
@@ -36,23 +36,20 @@ if (usercontext.user.is_profile_img_updated)
 
 if (!data)
 {
-  return <div className="py-8 w-[350px] mx-auto">
-  <div className="bg-gradient-to-br from-purple-600 to-indigo-800 rounded-lg shadow-md p-6 flex items-center space-x-4 transform transition-transform hover:scale-105">
-    <div className="w-16 h-16 rounded-full overflow-hidden">
-    <Avatar size='md' name={usercontext.user.username} src={usercontext.user.is_profile_img_updated ? new_src_img : usercontext.user.profile_img} className="w-full h-full object-cover profile-img">
-                        </Avatar>
-    </div>
-    <div className="flex flex-col flex-1 space-y-2">
-    
-      <div className="flex items-center justify-between">
-        <div className="text-purple-400 text-sm">Games: 0</div>
-        <div className="text-green-400 text-sm">Wins: 0</div>
-        <div className="text-yellow-400 text-sm">Losses: 0</div>
-      </div>
-      <div className="text-blue-400 text-xl">Win Rate: You Dont have Any Game Yet!</div>
-    </div>
-    
-  </div>
+  return <div className="live-game-card playbanner rounded-lg shadow-md p-6 flex flex-col justify-between">
+  <div className="w-16 h-16 rounded-full overflow-hidden">
+<Avatar size='md' name={usercontext.user.username} src={usercontext.user.is_profile_img_updated ? new_src_img : usercontext.user.profile_img} className="w-full h-full object-cover profile-img">
+                  </Avatar>
+</div>
+<div className="">
+
+<div className="flex items-center justify-between">
+<div className="text-purple-400 text-lg">Games: 0</div>
+<div className="text-green-400 text-lg">Wins: 0</div>
+<div className="text-yellow-400 text-lg">Losses: 0</div>
+</div>
+<div className="text-blue-400 text-xl mt-4">Win Rate: 0%</div>
+</div>
 </div>
 }
 
@@ -60,24 +57,24 @@ if (!data)
 const winRate = (data.totalwins / data.totalgames) * 100;
   return (
 
-    <div className="py-8 w-[350px] mx-auto">
-  <div className="bg-gradient-to-br from-purple-600 to-indigo-800 rounded-lg shadow-md p-6 flex items-center space-x-4 transform transition-transform hover:scale-105">
-    <div className="w-16 h-16 rounded-full overflow-hidden">
+
+
+    <div className="live-game-card playbanner rounded-lg shadow-md p-6 flex flex-col justify-between">
+        <div className="w-16 h-16 rounded-full overflow-hidden">
     <Avatar size='md' name={usercontext.user.username} src={usercontext.user.is_profile_img_updated ? new_src_img : usercontext.user.profile_img} className="w-full h-full object-cover profile-img">
                         </Avatar>
     </div>
-    <div className="flex flex-col flex-1 space-y-2">
+    <div className="">
     
-      <div className="flex items-center justify-between">
-        <div className="text-purple-400 text-sm">Games: {data.totalgames}</div>
-        <div className="text-green-400 text-sm">Wins: {data.totalwins}</div>
-        <div className="text-yellow-400 text-sm">Losses: {data.totalosses}</div>
-      </div>
-      <div className="text-blue-400 text-xl">Win Rate: {winRate}%</div>
+    <div className="flex items-center justify-between">
+      <div className="text-purple-400 text-lg">Games: {data.totalgames}</div>
+      <div className="text-green-400 text-lg">Wins: {data.totalwins}</div>
+      <div className="text-yellow-400 text-lg">Losses: {data.totalosses}</div>
     </div>
-    
+    <div className="text-blue-400 text-xl">Win Rate: { (winRate - Math.floor(winRate) < 0.5)? Math.floor(winRate) : Math.ceil(winRate)}%</div>
   </div>
-</div>
+      </div>
+//     
 
 
 
--- Frontend/app/(root)/Components/Home/Components/LiveGames.tsx
+++ Frontend/app/(root)/Components/Home/Components/LiveGames.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import '../Home.css'
+import BannerHome from './BannerHome';
+import HomeStats from './HomeStats';
 
 
 export default function LiveGames() {
@@ -7,35 +9,42 @@ export default function LiveGames() {
 
     <div className="live-games">
   
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
      
+    <BannerHome/>
 
-    <div className="live-game-card bg-gradient-to-br from-fuchsia-600 to-fuchsia-800 rounded-lg shadow-md p-6 flex flex-col justify-between">
-        <div className="text-white text-lg font-semibold mb-4">Training With Bot</div>
-        <p className="text-white">Enjoy Tranining with our ping pong Bot!</p>
-        <Link className="text-blue-400 font-semibold mt-4 rounded-lg inline-block" href="/Game/Ofline/BOT">Training Now </Link>
+    <HomeStats/>
+    <div className="live-game-card aibanner rounded-lg shadow-md p-6 ">
+
+    <h1 className="text-2xl font-bold mb-4">Training With Bot</h1>
+      <p className="text-md mb-4">Enjoy Tranining with our ping pong Bot!</p>
+      <Link className=" bg-gradient-to-br from-purple-600 to-indigo-800 font-semibold py-2 px-4 rounded-lg inline-block" href="/Game/Ofline/BOT">Get Started</Link>
+       
       </div>
 
 
-      <div className="live-game-card bg-gradient-to-br from-purple-600 to-purple-800 rounded-lg shadow-md p-6 flex flex-col justify-between">
-        <div className="text-white text-lg font-semibold mb-4">Watch Live Games</div>
-        <p className="text-white">Enjoy watching live ping pong matches!</p>
-        <Link className="text-blue-400 font-semibold mt-4 rounded-lg inline-block" href="/Stream">Watch Now</Link>
+
+      <div className="live-game-card bannerwatch rounded-lg shadow-md p-6  justify-between">
+        <div className="text-white text-lg font-semibold mb-4"></div>
+        <p className="text-white"></p>
+        <Link className=" bg-gradient-to-br from-purple-600 to-indigo-800 font-semibold mt-20 py-2 px-4 rounded-lg inline-block" href="/Stream">Watch Now</Link>
       </div>
   
       
-      <div className="live-game-card bg-gradient-to-br from-teal-600 to-teal-800 rounded-lg shadow-md p-6 flex flex-col justify-between">
-        <div className="text-white text-lg font-semibold mb-4">Start a Chat</div>
-        <p className="text-white">Connect with players and start chatting!</p>
-        <Link className="text-blue-400 font-semibold mt-4 rounded-lg inline-block" href="/community">Chat Now</Link>
+      <div className="live-game-card bannerchat rounded-lg shadow-md p-6  justify-between">
+        <div className="text-white text-lg font-semibold mb-4"></div>
+        <p className="text-white"></p>
+        <Link className=" bg-gradient-to-br from-purple-600 to-indigo-800 font-semibold mt-20 py-2 px-4 rounded-lg inline-block" href="/community">Chat Now</Link>
       </div>
   
       
-      <div className="live-game-card bg-gradient-to-br from-indigo-600 to-indigo-800 rounded-lg shadow-md p-6 flex flex-col justify-between">
-        <div className="text-white text-lg font-semibold mb-4">Find New Friends</div>
-        <p className="text-white">Discover new players to challenge!</p>
-        <Link className="text-blue-400 font-semibold mt-4 rounded-lg inline-block" href="/Users">Find Friends</Link>
+      <div className="live-game-card bannerfind rounded-lg shadow-md p-6  justify-between">
+        <div className="text-white text-lg font-semibold mb-4"></div>
+        <p className="text-white"></p>
+        <Link className=" bg-gradient-to-br from-purple-600 to-indigo-800 font-semibold mt-20 py-2 px-4 rounded-lg inline-block" href="/Users">Find Friends</Link>
       </div>
+
+
     </div>
   </div>
 
