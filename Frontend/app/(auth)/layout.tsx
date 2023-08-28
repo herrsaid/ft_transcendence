c16cce0bfc244aebcc39b9c5d7ace7f90f835542
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-08-28 18:00:21 +0100

    small change

--- Frontend/app/(auth)/layout.tsx
+++ Frontend/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Links from '../(root)/Components/Links/Links'
 import './../(root)/globals.css'
 
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     
     <html lang="en">
       <body>
+      <Links/>
               {children}
       </body>
     </html>
