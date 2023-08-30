16328115955cbd87da8e4eb3b91cdfe89db5860a
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-08-30 12:44:08 +0100

    some addition in the 2fa logic frontend

--- Frontend/app/(root)/Components/Header/Header.tsx
+++ Frontend/app/(root)/Components/Header/Header.tsx
@@ -18,8 +18,9 @@ export default  function Header()
     let new_src_img;
 
     const logout = async ()  => {
-   
+      
       document.cookie = 'access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      document.cookie = 'twofactortoken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 
       router.replace("/login");
     
