5bea47502afbac9806fa0c94bbbec77cc680aed2
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-10-07 10:06:31 +0100

    edit

--- backend/src/user/controllers/user.controller.ts
+++ backend/src/user/controllers/user.controller.ts
@@ -247,6 +247,19 @@ export class UserController {
     @Put('edit/avatar')
         @UseInterceptors(FileInterceptor('file',
         {
+
+            limits: {
+                fileSize: 1024 * 1024 * 4, 
+              },
+
+            fileFilter: (req, file, callback) => {
+                if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+                  callback(null, true);
+                } else {
+                    callback(null, false);
+                }
+              },
+
             storage: diskStorage({
                 destination: './uploadedFiles/avatars',
                 filename: (req, file, cb) => {
